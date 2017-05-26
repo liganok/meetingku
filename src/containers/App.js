ef60d2996da9638e60bc8d281dbe39e5e6ca93e8
Gan <liganok86@qq.com>
2017-05-26 19:43:58 +0800

    use rout v4

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,49 @@
 import {HashRouter, Switch, Route,Link } from 'react-router-dom'
 import React from 'react';
+import { connect } from 'react-redux';
+import { APP_LOAD, REDIRECT } from '../constants/actionTypes';
+import agent from '../agent';
+
 import Login from '../containers/Login';
 import Header from '../containers/Header';
-
 import Agenda from '../components/AgendaList';
 
+const mapStateToProps = state => ({
+  appLoaded: state.common.appLoaded,
+  appName: state.common.appName,
+  currentUser: state.common.currentUser,
+  redirectTo: state.common.redirectTo
+});
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: (payload, token) =>
+    dispatch({ type: APP_LOAD, payload, token, skipTracking: true }),
+  onRedirect: () =>
+    dispatch({ type: REDIRECT })
+});
+
 class App extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.redirectTo) {
+      this.context.router.replace(nextProps.redirectTo);
+      this.props.onRedirect();
+    }
+  }
+
+  componentWillMount() {
+    const token = window.localStorage.getItem('jwt');
+    if (token) {
+      agent.setToken(token);
+    }
+
+    this.props.onLoad(token ? agent.Auth.current() : null, token);
+  }
   render() {
     return (
       <div>
-        <Header/>
+        <Header
+          appName={this.props.appName}
+          currentUser={this.props.currentUser} />
         <Switch>
           <Route exact path='/' component={Login}/>
           <Route path='/login' component={Login}/>
@@ -20,4 +54,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,9 +1,61 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
-import Drawer from 'material-ui/Drawer';
+import {Drawer,Avatar} from 'material-ui';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import LockIcon from 'material-ui/svg-icons/action/lock-outline';
+import Divider from 'material-ui/Divider';
+import {HashRouter, Switch, Route,Link } from 'react-router-dom'
+import Agenda from '../components/AgendaList';
+
+
+const styles = {
+  main: {
+    marginLeft: '1em',
+  },
+  login: {
+    block:{
+      margin: '1em',
+      textAlign: 'center ',
+    },
+    button:{
+      margin: 8,
+    }
+  },
+  input: {
+    display: 'flex',
+  },
+};
+
+const LoggedOutView = props=>{
+  if(!props.currentUser){
+    return (
+      <div style={{ ...styles.login.block, }}>
+        <div style={{display:false}}>
+          <Avatar  icon={<LockIcon />} size={50} />
+        </div>
+        <RaisedButton label="Sign Up" primary={true} style={styles.login.button} />
+        <Divider />
+      </div>
+    );
+  }
+  return null;
+}
+
+const LoggedInView = props=>{
+  if(props.currentUser){
+    return (
+      <div style={{ ...styles.login.block, }}>
+        <div style={{display:false}}>
+          <Avatar  icon={<LockIcon />} size={50} />
+        </div>
+        <Divider />
+      </div>
+    );
+  }
+  return null;
+}
 
 class Header extends React.Component{
   constructor() {
@@ -23,18 +75,24 @@ class Header extends React.Component{
           iconClassNameRight="muidocs-icon-navigation-expand-more"
           onLeftIconButtonTouchTap={this.handleToggle}
         />
-        <Drawer
-          docked={false}
-          width={300}
-          open={this.state.open}
-          onRequestChange={(open) => this.setState({open})}
+        <Drawer style={styles.main}
+                docked={false}
+                width={300}
+                open={this.state.open}
+                onRequestChange={(open) => this.setState({open})}
         >
           <AppBar
             title="Test"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
             onLeftIconButtonTouchTap={this.handleToggle}
           />
-          <MenuItem onTouchTap={this.handleToggle}>Menu Item</MenuItem>
+
+          <LoggedOutView currentUser={this.props.currentUser} />
+          <LoggedInView currentUser={this.props.currentUser} />
+
+          <MenuItem onTouchTap={this.handleToggle}>
+            <Link to="/agenda">Agenda</Link>
+          </MenuItem>
           <MenuItem onTouchTap={this.handleToggle}>Menu Item 2</MenuItem>
         </Drawer>
       </div>
