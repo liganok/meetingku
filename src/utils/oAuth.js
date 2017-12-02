1e56edcf4064783d7a65c0bc0daf7da5a75c7d9a
Gan <liganok86@qq.com>
2017-12-02 22:49:54 +0800

    fix

--- src/utils/oAuth.js
+++ src/utils/oAuth.js
@@ -1,8 +1,8 @@
 
 function generateGoogleUrl() {
   let CLIENT_ID = '741761109516-i26a0o3l2hjbtp80q4rh4385deq62b33.apps.googleusercontent.com'
-  let REDIRECT_URI = 'http://localhost:3000/oauth/google'
-  let SCOPE = 'https://www.googleapis.com/auth/userinfo.profile'
+  let REDIRECT_URI = (process.env.NODE_ENV === 'development') ? 'http://localhost:3000/oauth/google' : 'https://www.meetingku.com/oauth/google'
+  let SCOPE = 'https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/userinfo.email'
   let STATE = ''
   let RESPONSE_TYPE = 'token'
   let oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth?'
@@ -11,6 +11,7 @@ function generateGoogleUrl() {
     'client_id='+CLIENT_ID,
     'scope=' + SCOPE,
     'state=' + STATE,
+    'include_granted_scopes='+true,
     'redirect_uri=' + REDIRECT_URI,
     'response_type=' + RESPONSE_TYPE
   ]
