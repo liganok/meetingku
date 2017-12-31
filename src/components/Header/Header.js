a2b462ec213ca15ae3c1481e392a47c736c0a060
Gan <liganok86@qq.com>
2017-12-31 22:12:03 +0800

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -30,55 +30,57 @@ function Header(props) {
         inProgress={inProgress}
         onActionToggle={onActionToggle}/>
       <Drawer open={isShowDrawer} onRequestClose={onActionToggle}>
-        <AppHeader
-          appName={appName}  
-          isShowRightButtons={false}
-          position="absolute"
-          onActionToggle={onActionToggle}
-        />
+        <div style={{width:250}}>
+          <AppHeader
+            appName={appName}
+            isShowRightButtons={false}
+            position="absolute"
+            onActionToggle={onActionToggle}
+          />
 
-        <Grid style={{ width: 250, paddingLeft: 10 }}>
-          <SLink to="/agenda">
-            <ListItem button onClick={onActionToggle}>
-              <ListItemIcon>
-                <ViewAgenda />
-              </ListItemIcon>
-              <ListItemText primary="Agenda" />
-            </ListItem>
-          </SLink>
-          <SLink to="/template">
-            <ListItem button onClick={onActionToggle}>
-              <ListItemIcon>
-                <Description />
-              </ListItemIcon>
-              <ListItemText primary="Template" />
-            </ListItem>
-          </SLink>
-          <SLink to="/trash">
-            <ListItem button onClick={onActionToggle}>
-              <ListItemIcon>
-                <Delete />
-              </ListItemIcon>
-              <ListItemText primary="Trash" />
-            </ListItem>
-          </SLink>
-          <SLink to="/setting">
-            <ListItem button onClick={onActionToggle}>
-              <ListItemIcon>
-                <Settings />
-              </ListItemIcon>
-              <ListItemText primary="Setting" />
-            </ListItem>
-          </SLink>
-          <SLink to="/help">
-            <ListItem button onClick={onActionToggle}>
-              <ListItemIcon>
-                <Help />
-              </ListItemIcon>
-              <ListItemText primary="Help" />
-            </ListItem>
-          </SLink>
-        </Grid>
+          <div>
+            <SLink to="/agenda">
+              <ListItem button onClick={onActionToggle}>
+                <ListItemIcon>
+                  <ViewAgenda />
+                </ListItemIcon>
+                <ListItemText primary="Agenda" />
+              </ListItem>
+            </SLink>
+            <SLink to="/template">
+              <ListItem button onClick={onActionToggle}>
+                <ListItemIcon>
+                  <Description />
+                </ListItemIcon>
+                <ListItemText primary="Template" />
+              </ListItem>
+            </SLink>
+            <SLink to="/trash">
+              <ListItem button onClick={onActionToggle}>
+                <ListItemIcon>
+                  <Delete />
+                </ListItemIcon>
+                <ListItemText primary="Trash" />
+              </ListItem>
+            </SLink>
+            <SLink to="/setting">
+              <ListItem button onClick={onActionToggle}>
+                <ListItemIcon>
+                  <Settings />
+                </ListItemIcon>
+                <ListItemText primary="Setting" />
+              </ListItem>
+            </SLink>
+            <SLink to="/help">
+              <ListItem button onClick={onActionToggle}>
+                <ListItemIcon>
+                  <Help />
+                </ListItemIcon>
+                <ListItemText primary="Help" />
+              </ListItem>
+            </SLink>
+          </div>
+        </div>
       </Drawer>
     </div>
   )
